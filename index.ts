2386f7df8d98f4359556b41ad8a59b602c343468
jhosscy <jhosscy@protonmail.com>
2025-03-18 01:34:49 +0000

    refactor(server): update port config and remove custom fetch handler

--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
 import homepage from './src/index.html';
 import qrscanner from './src/qrscanner.html';
 import qrgenerator from './src/qrgenerator.html';
-import header from './header.ts';
 
 const server = Bun.serve({
-  port: 10000,
+  port: process.env.PORT || 4321,
   routes: {
     '/': homepage,
     '/qrscanner': qrscanner,
     '/qrgenerator': qrgenerator
   },
-  fetch: async (req: Request) => {
-    const pathname = new URL(req.url).pathname;
-    const file = await Bun.file(`${Bun.cwd}/${pathname}`)
-    const ext = pathname.split('.').pop();
-    if (!(await file.exists())) {
-      return new Response('Archivo no encontrado', header({status: 404}));
-    }
-    return new Response(file, header({ext: ext}))
-  }
+  development: false
 });
